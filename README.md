ff262a5d0dcbe79bb6d2ba0fe533dcffd9abe3f4
Dyne Park <dynepark9111@gmail.com>
2023-01-02 20:55:31 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Boilerplate
+
+## Stack
+
+- A
+
+## Get Started
+
+## Install dependencies
+
+```
+yarn add
+```
+
+### Run application
+
+```
+yarn start
+```
+
+### Run tests
+
+```
+yarn test
+```
---
+++ src/__tests__/temp.test.ts
@@ -0,0 +1,17 @@
+import { sample } from "../services/temp";
+
+describe("Given", () => {
+  describe("When", () => {
+    it("then", () => {
+      let result = sample(1, 1);
+      expect(result).toBe(2);
+    });
+  });
+
+  describe("When", () => {
+    it("then", () => {
+      let result = sample(1, 1);
+      expect(result).toBe(2);
+    });
+  });
+});
